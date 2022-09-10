f6be4c2be622d7ce82e312f7b07017a9e1fc2d69
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-09-10 11:07:17 -0600

    Initial commit

---
+++ src/main/java/com/example/msgwebsocket/MsgWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgWebSocketApplication.class, args);
+    }
+
+}
